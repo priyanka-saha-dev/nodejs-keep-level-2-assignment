79d9c0154f2c29ead0965f464e8e46e8c2fdb915
Saha <404110@cognizant.com>
2019-02-15 19:54:41 +0530

    test cases (ongoing)

--- server/test/auth.spec.js
+++ server/test/auth.spec.js
@@ -1,9 +1,13 @@
 const expect = require('chai').expect;
 const { signJWTToken, verifyJWTToken } = require('../modules');
+const testConfig = require('./test.config');
+const uuidv1 = require('uuid/v1');
 
 describe('JWT Token test scenarios', function() {
 	before(function(done) { done(); });
 	after(function(done) { done(); });
+
+	let jwt;
 
 	it('Assert signing & verification methods exists and are valid', function() {
 		expect(signJWTToken).to.not.equal(undefined);
@@ -20,9 +24,29 @@ describe('JWT Token test scenarios', function() {
 	});
 
 	it('sign a token with valid payload, signature, secret and expiry time', function(done) { 
-		done() 
+		
+		signJWTToken(testConfig.JWTPayload, testConfig.JWTsecretkey, '10h', (err, token) => {
+			console.log('Token generated :', token);
+			expect(err).to.equal(null);
+			expect(token).to.not.equal(null);
+			jwt = token;
+
+			done();
+		});
+		
+		//done() 
 	});
+
 	it('verification of a valid signed token, must return same payload, which was passed', function(done) {
+
+		verifyJWTToken(jwt, testConfig.JWTsecretkey, (err, decoded) => {
+			console.log('Decoded value:', decoded);
+			expect(err).to.equal(null);
+			expect(token).to.not.equal(null);
+			
+			expect(decoded).to.deep.equal(testConfig.JWTPayload);
+		})
+
 	 done() 
 	});
 	it('verification a expired token, must return with appropriate error', function(done) {
