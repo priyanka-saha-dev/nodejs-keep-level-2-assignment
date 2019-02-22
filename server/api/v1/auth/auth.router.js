2b2d70b13d9a00409a4ed6cac24e653fbb927114
Saha <404110@cognizant.com>
2019-02-22 16:08:42 +0530

    hobbes

--- server/api/v1/auth/auth.router.js
+++ server/api/v1/auth/auth.router.js
@@ -7,10 +7,10 @@ router.post('/isAuthenticated', (req, res, next) => {
   try {
 
     auth.isUserAuthenticatedRouter(req, res).then((response) => {
-      res.status(response.status).send(response.message);
+      res.status(response.status).send(response);
     }).catch((error) => {
       //console.log('Promise rejected with', error);
-      res.status(error.status).send(error.message);
+      res.status(error.status).send(error);
     });
 
   } catch (err) {
